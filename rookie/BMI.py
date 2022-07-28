5ce44fa73809eb0e32475318053ed0c898bac24f
Shanchien Lee <102730245+ShanchienLee@users.noreply.github.com>
2022-07-28 17:09:40 +0800

    Update BMI.py

--- rookie/BMI.py
+++ rookie/BMI.py
@@ -6,4 +6,15 @@ weight = int(weight)
 
 BMI = round(weight/(height ** 2),2)
 
-print(f"您的BMI是：{BMI}")
+if BMI < 18.5:
+    print(f"您的BMI是：{BMI}(體重過輕)")
+elif BMI < 24:
+    print(f"您的BMI是：{BMI}(正常體位)")
+elif BMI < 27:
+    print(f"您的BMI是：{BMI}(體重過重)")
+elif BMI < 30:
+    print(f"您的BMI是：{BMI}(輕度肥胖)")
+elif BMI < 35:
+    print(f"您的BMI是：{BMI}(中度肥胖)") 
+else:
+    print(f"您的BMI是：{BMI}(重度肥胖)") 
