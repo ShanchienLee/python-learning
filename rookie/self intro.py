89fc56d188802670e586dd1ceefb0930a388a5e0
Shanchien Lee <102730245+ShanchienLee@users.noreply.github.com>
2022-07-28 16:32:38 +0800

    Create self intro.py

---
+++ rookie/self intro.py
@@ -0,0 +1,7 @@
+print("歡迎使用自我介紹產生器")
+name = input(" 請輸入您的名字")
+age = input("請輸入您的年齡")
+hobby = input("請輸入您的興趣")
+
+intro = "大家好，我是" + name + "我今年" + age + "歲，我的興趣是" + hobby + "，很高興認識大家！"
+print(intro)
